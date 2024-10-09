f3b8e5f9ef92ea3dfe0c5ae97c2928952cc31e33
Iliya Efanov <84502015+JeskoThe1@users.noreply.github.com>
2024-10-09 11:41:36 +0000

    Updated secrets and added langgraph to requirements.txt

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,14 +1,15 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st 
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
-NEO4J_URI = os.getenv("NEO4J_URI")
-NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+TAVILY_API_KEY = st.secrets["TAVILY_API_KEY"]
+NEO4J_URI = st.secrets["NEO4J_URI"]
+NEO4J_USERNAME = st.secrets["NEO4J_USERNAME"]
+NEO4J_PASSWORD = st.secrets["NEO4J_PASSWORD"]
 
 LOCAL_LLM = "llama3.1"
 GPT4_MODEL = "gpt-4o-mini"
